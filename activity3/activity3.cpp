77cbe28b1b348ab4f21483f39bdb2da33831efe9
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-04-02 18:37:24 +0700

    struct detail alamat

--- activity3/activity3.cpp
+++ activity3/activity3.cpp
@@ -1,3 +1,11 @@
 // activity3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
