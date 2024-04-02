0a3d6ae31e957e22e5fd82b9ea803c6373c15b33
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-04-02 19:23:58 +0700

    variable alamat

--- activity3/activity3.cpp
+++ activity3/activity3.cpp
@@ -26,3 +26,13 @@ int main()
 		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa :";
 		getline(cin, mhs[i].nama);
+
+		cout << "Alamat Mahasiswa :" << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t nama kota :";
+		cin >> mhs[i].alamat.kota;
+		cout << "umur mahasiswa :";
+		cin >> mhs[i].umur;
+
+	}
