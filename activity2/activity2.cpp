91240f2407f6a9369c43f22534f02d94a790affb
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-04-02 18:30:18 +0700

    memasukan struct Detail alamat

--- activity2/activity2.cpp
+++ activity2/activity2.cpp
@@ -1,3 +1,10 @@
 // activity2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
