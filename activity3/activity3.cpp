a8ca2bc051869698c229a59995af7e33576ce1f6
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-04-02 19:22:43 +0700

    variable data

--- activity3/activity3.cpp
+++ activity3/activity3.cpp
@@ -16,3 +16,10 @@ struct Mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs[3];
+	for (int i = 0; 1 < 3; i++) {
+		cout << "Data ke- " << (i + 1) << ":" << endl;
+		
