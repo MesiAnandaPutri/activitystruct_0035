0fab9d260daecf3837e893ea31d0cad7c5280379
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-04-02 18:33:44 +0700

    variable alamat

--- activity2/activity2.cpp
+++ activity2/activity2.cpp
@@ -23,3 +23,10 @@ int main()
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota: ";
+    cin >> mhs.alamat.kota;
+
